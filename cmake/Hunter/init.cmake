9aae59c824fb2207d3dd7aefe139c520ea82f229
simbahebinbo <lansheng228@163.com>
2024-08-31 22:59:51 +0800

    update

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -20,14 +20,13 @@ set(
         "Hunter passwords"
 )
 
-#set(
-#        HUNTER_CACHE_SERVERS
-#        "https://github.com/qdrvm/hunter-binary-cache"
-#        CACHE
-#        STRING
-#        "Binary cache server"
-#)
-set(HUNTER_USE_CACHE_SERVERS NO)
+set(
+        HUNTER_CACHE_SERVERS
+        "https://github.com/qdrvm/hunter-binary-cache"
+        CACHE
+        STRING
+        "Binary cache server"
+)
 
 include(${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake)
 
